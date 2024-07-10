b5f5759e7215881e72263cdbd4c3573e6148ee97
Kyrylo <deltaonlyone@gmail.com>
2024-07-11 00:27:09 +0300

    Added main info about bags & timer to next week

--- src/components/homePage/Home.js
+++ src/components/homePage/Home.js
@@ -1,11 +1,15 @@
 import styles from './Home.module.css';
 import NavBar from "./navBar/NavBar";
 import React from "react";
+import TopInfo from "./topInfo/TopInfo";
+import SaleTimer from "./saleTimer/SaleTimer";
 const Home = () => {
 
     return(
       <div className={`${styles['home']}`}>
           <NavBar></NavBar>
+          <TopInfo></TopInfo>
+          <SaleTimer></SaleTimer>
       </div>
     );
 }
