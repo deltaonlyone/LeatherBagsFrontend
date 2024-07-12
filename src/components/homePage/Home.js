b0e09ca8e7103a47c15cc95c75f3a13a4d7050b6
deltaonlyone <deltaonlyone@gmail.com>
2024-07-12 12:17:00 +0300

    fixed navbar name

--- src/components/homePage/Home.js
+++ src/components/homePage/Home.js
@@ -1,7 +1,7 @@
 import styles from './Home.module.css';
 import ItemInfo from "./itemInfo/ItemInfo";
 import ItemVideo from "./itemVideo/ItemVideo";
-import NavBar from "./navBar/NavBar";
+import NavBar from "./navbar/NavBar";
 import React from "react";
 import TopInfo from "./topInfo/TopInfo";
 import SaleTimer from "./saleTimer/SaleTimer";
@@ -9,14 +9,16 @@ import SaleTimer from "./saleTimer/SaleTimer";
 
 const Home = () => {
 
-    return(
-      <div className={`${styles['home']}`}>
-          <NavBar></NavBar>
-          <TopInfo></TopInfo>
-          <SaleTimer></SaleTimer>
-          <ItemInfo></ItemInfo>
-          <ItemVideo></ItemVideo>
-      </div>
+    return (
+        <div className={`${styles['home']}`}>
+            <NavBar></NavBar>
+            <TopInfo></TopInfo>
+            <SaleTimer></SaleTimer>
+            <ItemInfo></ItemInfo>
+            <ItemVideo></ItemVideo>
+            <SaleTimer></SaleTimer>
+            <NavBar></NavBar>
+        </div>
     );
 }
 
