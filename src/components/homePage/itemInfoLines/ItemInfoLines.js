8f6986e5e4aed5499cf2476a30b9b750355ec06c
deltaonlyone <deltaonlyone@gmail.com>
2024-08-03 23:14:23 +0300

    Added count up for info lines

--- src/components/homePage/itemInfoLines/ItemInfoLines.js
+++ src/components/homePage/itemInfoLines/ItemInfoLines.js
@@ -1,32 +1,39 @@
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
+import CountUp from 'react-countup';
 import styles from './ItemInfoLines.module.css';
 
-const ItemInfoLines = () => {
+const StatItem = ({ end, text }) => {
+    const { ref, inView } = useInView({
+        triggerOnce: true,  // Тригер тільки один раз
+        threshold: 0.1,     // Поріг видимості (10% видимо)
+    });
 
+    return (
+        <div className={styles.statItem} ref={ref}>
+            <div className={styles.numbs}>
+                {inView ? <CountUp end={end} duration={2} /> : '0'}
+                +
+            </div>
+            <div className={styles.secondaryText}>{text}</div>
+        </div>
+    );
+};
+
+const ItemInfoLines = () => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.textWrap}>
                 КУПУВАТИ У НАС КОМФОРТНО, БЕЗПЕЧНО ТА ВИГІДНО
             </div>
             <div className={styles.statsWrap}>
-                <div className={styles.statItem}>
-                    <div className={styles.numbs}>&nbsp;500+</div>
-                    <div className={styles.secondaryText}>Задоволених покупців</div>
-                </div>
-                <div className={styles.statItem}>
-                    <div className={styles.numbs}>&nbsp;10+</div>
-                    <div className={styles.secondaryText}>Років на ринку</div>
-                </div>
-                <div className={styles.statItem}>
-                    <div className={styles.numbs}>&nbsp;20+</div>
-                    <div className={styles.secondaryText}>Досвічених фахівців</div>
-                </div>
-                <div className={styles.statItem}>
-                    <div className={styles.numbs}>&nbsp;40+</div>
-                    <div className={styles.secondaryText}>Моделей</div>
-                </div>
+                <StatItem end={500} text="Задоволених покупців" />
+                <StatItem end={10} text="Років на ринку" />
+                <StatItem end={20} text="Досвічених фахівців" />
+                <StatItem end={40} text="Моделей" />
             </div>
         </div>
     );
-}
+};
 
 export default ItemInfoLines;
